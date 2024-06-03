10704b1db74431be82a4da33a7f26c2d78e853cd
Akash Patil <akashgpatil8806@example.com>
2024-06-03 10:22:53 +0530

    prime number

--- 20_primeNumber.js
+++ 20_primeNumber.js
@@ -1,16 +1,31 @@
-// this is the prime number
-let num=17;
-var isPrime=false;
-for(let i=2;i<11;i++){
-    if(num%i==0){
-        isPrime=false;
-        console.log(`is not prime number`);
-        break;
+// // this is the prime number
+// let num=19;
+// var isPrime=false;
+// for(let i=2;i<11;i++){
+//     if(num%i==0){
+//         isPrime=false;
+//         console.log(`is  prime number`);
+//         break;
+//     }
+// }
+// if(isPrime){
+//     console.log("the number is prime ")
+// }
+// else{
+//     console.log("this is not prime ")
+// }
+
+function prime(n) {
+  if (n < 2) 
+    return `${n} is not prime number`;
+
+    for (let i = 2; i < n; i++) {
+      if (n % i == 0) {
+        return `${n} is not prime number`;
+      }
     }
-}
-if(isPrime){
-    console.log("the number is prime ")
-}
-else{
-    console.log("this is not prime ")
-}
+    return `${n} is prime number`;
+  }
+  
+
+console.log(prime(10));
