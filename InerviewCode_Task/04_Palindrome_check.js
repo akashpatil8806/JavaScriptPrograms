e11a2238543f59ab513ec058f466a54b7e63cbd1
Akash Patil <akashgpatil8806@example.com>
2025-12-22 20:34:13 +0530

    program add

---
+++ InerviewCode_Task/04_Palindrome_check.js
@@ -0,0 +1,15 @@
+// check the given string is palindrome or not
+
+function CheckPalindrome(str) {
+  let start = 0;
+  let end = str.length - 1;
+  while (start < end) {
+    if (str[start] !== str[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+  return true;
+}
+console.log(CheckPalindrome("mdam"));
