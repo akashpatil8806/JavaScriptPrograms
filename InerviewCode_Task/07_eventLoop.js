73371060190e718532b9441993a88a0adb9b9748
Akash Patil <akashgpatil8806@example.com>
2026-04-06 22:17:48 +0530

    add event loop code

---
+++ InerviewCode_Task/07_eventLoop.js
@@ -0,0 +1,11 @@
+//The event loop handles asynchronous operations in JavaScript by managing:
+
+//Call Stack
+//Callback Queue
+//Web APIs
+
+console.log("this the event loop");
+setTimeout(() => {
+  console.log("this setTimeOut");
+}, 1000);
+console.log("End of the code.....");
